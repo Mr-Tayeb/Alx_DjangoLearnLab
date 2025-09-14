ce9f3be2c4a214652411e6c990a15e6e8c8cfc66
Mr-Tayeb <abijoue.ta@gmail.com>
2025-09-14 21:48:19 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("register/", views.register, name="register"),  # you need to create this view in views.py
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+    path('books/add/', views.add_book, name="add_book/"),
+    path('books/edit/<int:book_id>/', views.edit_book, name="edit_book/"),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'), 
 ]
