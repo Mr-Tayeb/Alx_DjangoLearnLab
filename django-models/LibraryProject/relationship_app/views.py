5f07c7ead1d01c054f9939f7b0709b4c4160c259
Mr-Tayeb <abijoue.ta@gmail.com>
2025-09-14 20:44:11 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Create your views here.
+# list all books
+def list_books(request):
+    books = Book.objects.all()
+    output = "\n".join([f"{book.title} - {book.author.name}" for book in books])
+    return HttpResponse(output, content_type="text/plain")
+
+
+# library details with books
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
