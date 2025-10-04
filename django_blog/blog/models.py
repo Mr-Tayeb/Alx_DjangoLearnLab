2732bc1a85808959a7987a5700b3262321c30525
Mr-Tayeb <abijoue.ta@gmail.com>
2025-10-04 21:22:18 +0100

    import User model
    I add the line "from django.contrib.auth.models import User", and delete class User.

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,4 +10,5 @@ class Post(models.Model):
     author = models.ForeignKey('User', on_delete=models.SET_DEFAULT, default='UNKNOWN',)
 
 class User(models.Model):
+
     author = models.CharField(max_length=200)
