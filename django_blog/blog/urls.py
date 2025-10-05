acb636a22093484d66be2a501f51a2aa5468d85f
Mr-Tayeb <abijoue.ta@gmail.com>
2025-10-05 23:55:38 +0100

    Add update and delete paths

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path("login/", views.login, name="login"),
     path("register/", views.register, name="register"),
     path("profile/", views.profile, name="profile"),
+
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(template_name='blog/post_confirm_delete.html'), name='post-delete'),
 ]
 
