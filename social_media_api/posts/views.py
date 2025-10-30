ce6532dc15599409ca0d500ae70a4994973863f5
Tayeb <abijoue.ta@gmail.com>
2025-10-30 20:20:06 +0100

    Update viewsets

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 from .serializers import *
 from .models import *
 
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 
 # Create your views here.
 
-class PostViewSet(ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class CommentViewSet(ModelViewSet):
+class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
